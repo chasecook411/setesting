811fb22c0147b9fc46e6ffda87bbc8e15314a0ee
chasecook411 <lccook@memphis.edu>
2017-10-02 17:13:53 -0500

    my third branch

--- stuff.py
+++ stuff.py
@@ -6,5 +6,5 @@ r = requests.get('https://www.google.com')
 print(r.status_code)
 
 # Its better if we print some of the output from the site
-
-print(r.text)
+# be more descriptive about what you're printing
+print("This is the text: " + r.text)
